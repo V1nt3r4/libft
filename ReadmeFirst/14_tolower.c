43556f946bab3276b8488cc3d96f50e8f80fdafe
Ariel <129072087+V1nt3r4@users.noreply.github.com>
2024-10-17 13:37:52 +0200

    Create 14_tolower.c

---
+++ ReadmeFirst/14_tolower.c
@@ -0,0 +1,29 @@
+# TOLOWER 
+## Prototipo
+``` int	ft_tolower(int c) ```
+
+### Filosofemos
+Imagina que tolower es como un mago que convierte letras grandes (mayúsculas) en letras pequeñas (minúsculas) con un toque suave y mágico.
+
+**¿Qué hace tolower?**
+tolower convierte un carácter en su equivalente en minúscula, si es una letra mayúscula. Si ya es minúscula o no es una letra, lo deja igual.
+
+## Cómo funciona:
+**Inicio**: Tienes un carácter cualquiera, como 'A'.
+
+**El Mago Transformador**: tolower revisa el carácter y pregunta: "¿Eres una letra mayúscula?"
+
+### Conjuro:
+
+- Si el carácter es una letra mayúscula ('A' a 'Z'), el mago lo transforma en la letra minúscula correspondiente ('a' a 'z').
+
+- Si el carácter no es una letra mayúscula, el mago no hace nada y lo deja igual.
+
+**Resultado**: Devuelve el carácter (ya sea transformado o no).
+
+### Ejemplo:
+>Carácter: 'A'
+
+>Proceso: El mago dice: "Es mayúscula, ¡abracadabra!", y lo transforma en 'a'.
+
+**Resultado**: *'a'*
